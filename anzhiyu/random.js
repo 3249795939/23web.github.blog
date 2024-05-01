3c86e608322cba893bc909fb2b422c4e518b83d0
xu <3249795939@qq.com>
2024-05-01 22:14:18 +0800

    all

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/01/hello-world/","2024/05/01/post-title-with-whitespace/"];function toRandomPost(){
+var posts=["2024/05/01/post-title-with-whitespace/","2024/05/01/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
